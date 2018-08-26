fcade5566f8922a175076bc2ba185d953d99efb5
t_nakajima <t_nakajima@bbmedia.co.jp>
2018-08-26 21:56:26 +0900

    make template and router

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Auth::routes();
-
+//
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function()
+{
+    Route::resource('posts', 'Admin\PostsController');
+});
